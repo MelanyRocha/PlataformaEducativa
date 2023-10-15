b9504faa524d01363f51a7019a94706a96336f07
Catiel <oliver.alandiaa@gmail.com>
2023-10-15 18:48:07 -0400

    conexion a la base de datos

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+// Conexión a la base de datos Mysql
+try {
+    $pdo = new PDO('mysql:host=database-plataformaedu.cclomwjbpd4h.sa-east-1.rds.amazonaws.com;port=3306;dbname=Base', 'admin', 'admin12345');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Sí ocurre algún error, enviar un mensaje de error al cliente
+    header('Content-Type: application/json');
+    echo json_encode(['error' => $e->getMessage()]);
+    exit();
+}
+
