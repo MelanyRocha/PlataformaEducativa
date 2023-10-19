5e3239d16b77ab3a40add787189f33bcaa5c8680
MelanyRocha <melanyrochaferrufino@gmail.com>
2023-10-19 18:17:04 -0400

    Estoy subiendo el JS que inserta el nombre de la materia en  la ventana contenido html

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const materiasList = document.querySelectorAll("#materias li a");
-    const contenidoMateria = document.querySelector("#contenido-materia"); // Cambia el selector según tu HTML
-
-    materiasList.forEach((materia) => {
-        materia.addEventListener("click", function (e) {
-            e.preventDefault();
-            const urlMateria = materia.getAttribute("href");
-            cargarContenidoMateria(urlMateria);
-        });
-    });
-
-    function cargarContenidoMateria(url) {
-        fetch("tu-archivo-php.php?url=" + encodeURIComponent(url)) // Cambia la URL de tu archivo PHP
-            .then((response) => response.text())
-            .then((data) => {
-                contenidoMateria.innerHTML = data;
-            })
-            .catch(() => {
-                contenidoMateria.innerHTML = "Error al cargar el contenido de la materia.";
-            });
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const contenido = document.getElementById("contenido");
+    const url = new URL(window.location.href);
+    const materia = url.searchParams.get("materia");
+    contenido.textContent = "Contenido de " + materia;
 });
