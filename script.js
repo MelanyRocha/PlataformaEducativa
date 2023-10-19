1531b212ca46af42bd07f8bd6d00438ba39957bd
MelanyRocha <melanyrochaferrufino@gmail.com>
2023-10-19 10:49:45 -0400

    Estoy subiendo las correcciones y mejoras en html,css y JS

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtén la lista de materias y el área de contenido
-    const materiasList = document.getElementById("materias").querySelector("ul");
-    const contenidoMateria = document.querySelector(".col-8");
+document.addEventListener("DOMContentLoaded", function () {
+    const materiasList = document.querySelectorAll("#materias li a");
+    const contenidoMateria = document.querySelector("#contenido-materia"); // Cambia el selector según tu HTML
 
-    // Agrega un manejador de eventos a los elementos de la lista de materias
-    materiasList.addEventListener("click", function(event) {
-        if (event.target.tagName === "A") {
-            event.preventDefault();
-
-            // Carga el contenido de la materia seleccionada en el área de contenido
-            const url = event.target.getAttribute("href");
-            fetch(url)
-                .then(response => response.text())
-                .then(data => {
-                    contenidoMateria.innerHTML = data;
-                })
-                .catch(error => {
-                    console.error("Error al cargar el contenido de la materia", error);
-                });
-        }
+    materiasList.forEach((materia) => {
+        materia.addEventListener("click", function (e) {
+            e.preventDefault();
+            const urlMateria = materia.getAttribute("href");
+            cargarContenidoMateria(urlMateria);
+        });
     });
+
+    function cargarContenidoMateria(url) {
+        fetch("tu-archivo-php.php?url=" + encodeURIComponent(url)) // Cambia la URL de tu archivo PHP
+            .then((response) => response.text())
+            .then((data) => {
+                contenidoMateria.innerHTML = data;
+            })
+            .catch(() => {
+                contenidoMateria.innerHTML = "Error al cargar el contenido de la materia.";
+            });
+    }
 });
