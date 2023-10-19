07f148e7ec12068dcdd914c6d5c2e4f1ee0c844a
fabritheon <carlosfabrizhioc@gmail.com>
2023-10-19 17:15:04 -0400

    Mejorar Conexion

--- tareas.php
+++ tareas.php
@@ -1,18 +1,16 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=database-plataformaedu.cclomwjbpd4h.sa-east-1.rds.amazonaws.com;port=3306;dbname=Plataformaedu', 'admin', 'admin12345');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo json_encode(['error' => $e->getMessage()]);
-    exit();
-}
 
-$query = "SELECT * FROM TareasPendientes";
-$result = $pdo->query($query);
-$tareas = $result->fetchAll(PDO::FETCH_ASSOC);
 
+// Preparar la consulta SQL para obtener todas las tareas pendientes
+global $pdo;
+$sql = 'SELECT nombre, entrega_dia, entrega_hora FROM TareasPendientes';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+// Obtener todas las tareas pendientes como un arreglo asociativo
+$tareasPendientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Enviar las tareas pendientes al cliente como JSON
 header('Content-Type: application/json');
-echo json_encode($tareas);
-
-
+echo json_encode($tareasPendientes);
 
