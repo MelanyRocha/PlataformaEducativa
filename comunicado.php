6c2063f995cbc0d939c6e84930d0977131ba34be
fabritheon <carlosfabrizhioc@gmail.com>
2023-10-19 22:30:25 -0400

    concluir proyecto

--- comunicado.php
+++ comunicado.php
@@ -5,7 +5,7 @@ require_once 'conexion.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $comment = $_POST['comment'];
 
-    $stmt = $pdo->prepare("INSERT INTO comentarios (texto) VALUES (:comment)");
+    $stmt = $pdo->prepare("INSERT INTO Plataformaedu.comentarios (texto) VALUES (:comment)");
     $stmt->bindParam(':comment', $comment);
 
     if ($stmt->execute()) {
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Código para obtener comentarios
     $comments = [];
-    $result = $pdo->query("SELECT texto FROM comentarios ORDER BY id DESC");
+    $result = $pdo->query("SELECT texto FROM Plataformaedu.comentarios ORDER BY id DESC");
 
     if ($result) {
         while ($row = $result->fetch()) {
