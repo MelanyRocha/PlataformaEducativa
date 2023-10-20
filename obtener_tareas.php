f8d82b9b1a77402b43f88c6e76562a77d6752688
fabritheon <carlosfabrizhioc@gmail.com>
2023-10-19 21:34:46 -0400

    mejoras

--- obtener_tareas.php
+++ obtener_tareas.php
@@ -1,12 +1,6 @@
 <?php
-try {
-    $pdo = new PDO('mysql:host=database-plataformaedu.cclomwjbpd4h.sa-east-1.rds.amazonaws.com;port=3306;dbname=Plataformaedu', 'admin', 'admin12345');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    header('Content-Type: application/json');
-    echo json_encode(['error' => $e->getMessage()]);
-    exit();
-}
+global $pdo;
+include 'conexion.php';
 
 $query = "SELECT nombre, entrega_dia, entrega_hora FROM TareasPendientes";
 $result = $pdo->query($query);
@@ -18,5 +12,7 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 
 header('Content-Type: application/json');
 echo json_encode($tareasPendientes);
-?>
+
+
+
 
